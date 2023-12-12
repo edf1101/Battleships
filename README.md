2c12036fe574369ab512fe591abf103a1366c9aa
Ed <edfillingham1@gmail.com>
2023-12-12 13:10:12 +0000

    Fixing pt 2

---
+++ README.md
@@ -0,0 +1,70 @@
+# ECM1400 Battleships
+
+## Introduction
+A python application to simulate a Battleships game against an AI in both a command line interface and a Flask GUI.
+
+## Coursework Self assessment
+All features required by specification are complete.
+### Additional 
+#### Entry Screen
+The user can select the game settings (size, difficulty) on an Entry page.
+Game screen has a return to menu button so games can be replayed
+#### Storm Mode
+When this is turned on The board will shift 1 space each turn in a predetermined direction
+#### HTML Template sunken ships
+If a ship has been sunk, the main.py script sends a list of sunk positions for
+either the AI or Player to the HTML code, and it colours those tiles green
+#### AI attacking
+There are 5 AI difficulties [code here](battleships/advanced_ai.py)
+0. Pure Random Guessing
+1. Random but it won't guess same space twice
+2. Guessing a random position around unsunk hits
+3. Trying to guess in a line around unsunk hits
+4. Guesses in a line if there are unsunk hits, if none it calculates the probability of a ship being in any 
+unguessed tile and guesses there
+
+Wrote [a script](battleships/ai_comparison.py) to test AI's against each other
+
+<img src="Images/AI scoring 2.png" alt="drawing" width="500"/>
+<img src="Images/AI scoring image.png" alt="drawing" width="500"/> 
+
+## Prerequisites
+- Flask must be installed ```pip install Flask```
+- numpy must be installed ```pip install numpy```
+- For testing pytest must be installed ```pip install pytest``` 
+- Pytest plugins must be installed```pip install pytest-depends``` ```pip install pytest-cov```
+- Python version must be ≥ 3.10 due to ```|``` symbol in typehinting
+
+_Version used in running = Python 3.11.4_
+
+## Installation
+Run in Terminal ``` pip install Flask```
+
+## Getting Started
+There are 3 modes to run the game:
+- Run the [game_engine file](battleships/game_engine.py) as a module for a CLI game against yourself
+- Run the [mp_game_engine file](battleships/mp_game_engine.py) for a CLI game against an AI
+- Run the [main file](battleships/main.py) for a full GUI game
+
+
+## Documentation
+[Code documentation can be found here](battleships/docs/index.html)
+
+## Testing
+If pytest & plugins are installed then you can test in 2 ways.
+
+_Note scripts like game_engine.py or main.py don't have full coverage as some functions require input statements which
+my unittests were not intended to mock_
+1. Use an IDE like PyCharm to run the tests manually
+2. Navigate to the tests folder in the project (from the root directory it would be
+```cd tests``` then run ```pytest``` )
+
+## Details
+#### License
+MIT License: [found here](LICENSE)
+
+#### Authors
+- Ed Fillingham
+
+#### Source
+Code hosted (privately) at https://github.com/edf1101/Battleships/
---
+++ battleships/components.py
@@ -0,0 +1,263 @@
+"""
+Basic functions used mainly for setting up games
+"""
+
+import copy
+import random
+import json
+
+
+def initialise_board(size: int = 10) -> list[list]:
+    """
+    Creates a blank board of size 'size'
+    :param size: How big the board should be in both x and y directions
+    :return: An empty list of lists (filled with None values)
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size should be of type int")
+
+    if size < 1:
+        raise ValueError("Size must be ≥ 1")
+
+    board = []
+    for _ in range(size):
+        row = [None for x in range(size)]
+        board.append(row)
+    return board
+
+
+def create_battleships(filename: str = "battleships.txt") -> dict[str, int]:
+    """
+    This function extracts the battleship data from a file
+    :param filename: The file to read ship data from
+    :return: A dictionary with the ships' names and sizes
+    """
+
+    if not isinstance(filename, str):
+        raise TypeError('filename parameter should be a string')
+
+    try:
+        with open(filename, 'r', encoding="utf-8") as file:
+            data = file.read()
+    except FileNotFoundError as ex:
+        raise ValueError(f"filename {filename} doesn't exist") from ex
+
+    data = data.split('\n')  # each new line should be data entry
+
+    battleships = {}
+    for item in data:
+        name, size = item.split(':')
+
+        try:  # Check the sizes are ints
+            size = int(size.strip())
+        except ValueError as e:
+            raise ValueError("battleships.txt error one value isn't of type int") from e
+
+        battleships[str(name)] = size
+
+    return battleships
+
+
+def try_place_ship(board: list[list],
+                   ship_name: str,
+                   ship_size: int,
+                   position: tuple[int, int],
+                   orientation: str) -> list[list[str | None]] | None:
+    """
+    Used for the random placement method in place_battleships function
+    :param board: A list of lists representing the board
+    :param ship_name: The name of the ship being placed
+    :param ship_size: The size of the ship being placed
+    :param position: starting position of the ship
+    :param orientation: what direction the ship faces  'v' = down, 'h' = right
+    :return: None if the placement is invalid, else the updated board list of lists
+    """
+
+    # Check for incorrect parameters
+    if (not isinstance(orientation, str) or not isinstance(ship_size, int)
+            or not isinstance(ship_name, str)
+            or not isinstance(board, list)):
+        raise TypeError('Incorrect argument type')
+
+    # Check for incorrect board argument
+    for row in board:
+        if not isinstance(row, list):
+            raise TypeError('board argument incorrect')
+
+    # Check for incorrect position argument
+    if (not isinstance(position, tuple) or not isinstance(position[0], int)
+            or not isinstance(position[1], int)):
+        raise TypeError('position argument incorrect')
+
+    # Check orientation is correct
+    if orientation not in ['v', 'h']:
+        raise ValueError('orientation should be v or h')
+
+    modify_board = copy.deepcopy(board)  # so it doesn't modify the board passed through parameters
+    for _ in range(ship_size):
+
+        if 0 <= position[0] < len(board) and 0 <= position[1] < len(board):
+            pass  # inside board
+        else:
+            return None  # out of bounds of the board
+
+        if board[position[1]][position[0]] is None:
+            pass  # empty space
+        else:
+            return None  # on another ship
+
+        modify_board[position[1]][position[0]] = ship_name
+
+        if orientation == 'v':  # Down
+            position = (position[0], position[1] + 1)
+
+        elif orientation == 'h':  # Right
+            position = (position[0] + 1, position[1])
+
+    return modify_board
+
+
+def place_battleships(board: list[list],
+                      ships: dict[str, int],
+                      algorithm: str = 'simple') -> list[list[str]]:
+    """
+    Places all the ships onto the board using a specified placement algorithm
+    :param board: A list of lists representing the board
+    :param ships: A dictionary of ships in the game and their sizes
+    :param algorithm: either 'simple', 'random' or 'custom'
+    :return: A list of lists representing the board, with tiles filled where ships are
+    """
+
+    # Error checking to see if any of the arguments are present but bad
+    if not isinstance(board, list) or not isinstance(ships, dict):
+        raise TypeError('parameter type error')
+
+    if len(board) == 0 or len(board[0]) == 0:
+        raise ValueError('Board parameter is of size 0')
+
+    if len(ships) == 0:
+        raise ValueError('ships parameter is of size 0')
+
+    # Basic placement algorithm as seen in specification
+    if algorithm == 'simple':
+
+        row = 0
+        for ship_name, ship_size in ships.items():
+
+            for x in range(ship_size):
+                board[row][x] = ship_name
+            row += 1  # go down a row
+
+        return board
+
+    # Ships will be placed with random position + orientation, as long as they fit
+    if algorithm == 'random':
+        for ship_name, ship_size in ships.items():
+
+            # First guess at a position
+            position = (random.randrange(0, len(board)), random.randrange(0, len(board)))
+            orientation = random.choice(['v', 'h'])
+            potential_placement = try_place_ship(board, ship_name, ship_size, position, orientation)
+
+            while potential_placement is None:  # try random spots until one is valid
+                position = (random.randrange(0, len(board)), random.randrange(0, len(board)))
+                orientation = random.choice(['v', 'h'])
+                potential_placement = try_place_ship(board,
+                                                     ship_name,
+                                                     ship_size,
+                                                     position,
+                                                     orientation)
+
+            board = potential_placement
+        return board
+
+    # Ships will be placed with a position and orientation as specified in a JSON file
+    if algorithm == 'custom':
+
+        with open('placement.json', 'r', encoding="utf-8") as file:
+            json_data = json.loads(file.read())
+
+        ship_names = list(json_data.keys())
+
+        for ship_name in ship_names:
+
+            # example file had a dict of ship_name : [position.x,position.y,orientation]
+            ship_position = (int(json_data[ship_name][0]), int(json_data[ship_name][1]))
+            ship_orientation = json_data[ship_name][2]
+
+            potential_placement = try_place_ship(board, ship_name,
+                                                 ships[ship_name],
+                                                 ship_position,
+                                                 ship_orientation)
+            if potential_placement is None:
+                raise ValueError("Supplied JSON data doesn't fit in the board")
+
+            board = potential_placement
+
+        return board
+
+    raise ValueError('Invalid Argument for algorithm parameter')
+
+
+def in_board(location: tuple[int, int], board: list[list]) -> bool:
+    """
+    Returns whether a point is inside the board or not
+    :param location: The point to query
+    :param board: Reference to the board so we can find it's size
+    :return: Whether its inside
+    """
+
+    if not isinstance(board, list) or not isinstance(location, tuple):
+        raise TypeError('parameter type error')
+
+    if not isinstance(location[0], int) or not isinstance(location[1], int):
+        raise TypeError('incorrect location parameter format')
+
+    if len(location) != 2:
+        raise ValueError('incorrect location parameter length')
+
+    inside = 0 <= location[0] < len(board) and 0 <= location[1] < len(board)
+    return inside
+
+
+def get_positions_by_name(board: list[list], names: list[str]) -> list[tuple[int, int]]:
+    """
+    Returns a list of all the tuple positions on the board that are occupied by a ship
+    with name in list names. Primarily used for finding sunk ships
+    :param board: The board to search in
+    :param names: The names of the ships to look for
+    :return: A list of the positions with name in list 'names'
+    """
+    # Check parameters
+    if not isinstance(board, list) or not isinstance(names, list):
+        raise TypeError('parameters are incorrect type')
+    # Check that each row of the board list is also a list
+    for row in board:
+        if not isinstance(row, list):
+            raise TypeError('parameter board is not a list of lists')
+
+    positions = []
+    for row_idx, row in enumerate(board):
+        for idx, cell in enumerate(row):
+
+            if cell in names:  # If the cell name is in names
+                positions.append((idx, row_idx))  # Append the tuple of its coordinates
+
+    return positions
+
+
+def get_sunken_ships(player_data: dict) -> list[tuple[int, int]]:
+    """
+    Calculate which of a player's ships we have sunk based on its original board
+    :param player_data: The dict containing boards, ships etc.
+    :return: A list of positions that have been sunk
+    """
+    if ('ships' not in player_data or 'original_board' not in player_data
+            or not isinstance(player_data, dict)):
+        raise ValueError('incomplete player data variable')
+
+    sunk_ship_types = [k for k, v in player_data['ships'].items() if v == 0]
+    sunken_places = get_positions_by_name(player_data['original_board'],
+                                          sunk_ship_types)
+    return sunken_places
